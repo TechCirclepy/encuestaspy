dc361a12230cff11bb15363a9b163014543ffe7a
Hernan <hernachaparro@gmail.com>
2018-08-22 14:51:28 -0400

    preview de imagenes

--- app/Http/Controllers/RealizarEncuestaController.php
+++ app/Http/Controllers/RealizarEncuestaController.php
@@ -62,6 +62,76 @@ class RealizarEncuestaController extends Controller
         $encuesta->opcion8 = $request->opcion8;
         $encuesta->opcion9 = $request->opcion9;
         $encuesta->opcion10 = $request->opcion10;
+        if(Input::hasFile('imagen1')) {
+            $file=Input::file('imagen1');
+            Image::make($request->file('imagen1'))
+                ->resize(144, 145)
+                ->save(public_path().'/imagenes/encuestas/' . $file->getClientOriginalName());
+            $encuesta->imagen1=$file->getClientOriginalName();
+        }
+        if(Input::hasFile('imagen2')) {
+            $file=Input::file('imagen2');
+            Image::make($request->file('imagen2'))
+                ->resize(144, 145)
+                ->save(public_path().'/imagenes/encuestas/' . $file->getClientOriginalName());
+            $encuesta->imagen2=$file->getClientOriginalName();
+        }
+        if(Input::hasFile('imagen3')) {
+            $file=Input::file('imagen3');
+            Image::make($request->file('imagen3'))
+                ->resize(144, 145)
+                ->save(public_path().'/imagenes/encuestas/' . $file->getClientOriginalName());
+            $encuesta->imagen3=$file->getClientOriginalName();
+        }
+        if(Input::hasFile('imagen4')) {
+            $file=Input::file('imagen4');
+            Image::make($request->file('imagen4'))
+                ->resize(144, 145)
+                ->save(public_path().'/imagenes/encuestas/' . $file->getClientOriginalName());
+            $encuesta->imagen4=$file->getClientOriginalName();
+        }
+        if(Input::hasFile('imagen5')) {
+            $file=Input::file('imagen5');
+            Image::make($request->file('imagen5'))
+                ->resize(144, 145)
+                ->save(public_path().'/imagenes/encuestas/' . $file->getClientOriginalName());
+            $encuesta->imagen5=$file->getClientOriginalName();
+        }
+        if(Input::hasFile('imagen6')) {
+            $file=Input::file('imagen6');
+            Image::make($request->file('imagen6'))
+                ->resize(144, 145)
+                ->save(public_path().'/imagenes/encuestas/' . $file->getClientOriginalName());
+            $encuesta->imagen6=$file->getClientOriginalName();
+        }
+        if(Input::hasFile('imagen7')) {
+            $file=Input::file('imagen7');
+            Image::make($request->file('imagen7'))
+                ->resize(144, 145)
+                ->save(public_path().'/imagenes/encuestas/' . $file->getClientOriginalName());
+            $encuesta->imagen7=$file->getClientOriginalName();
+        }
+        if(Input::hasFile('imagen8')) {
+            $file=Input::file('imagen8');
+            Image::make($request->file('imagen8'))
+                ->resize(144, 145)
+                ->save(public_path().'/imagenes/encuestas/' . $file->getClientOriginalName());
+            $encuesta->imagen8=$file->getClientOriginalName();
+        }
+        if(Input::hasFile('imagen9')) {
+            $file=Input::file('imagen9');
+            Image::make($request->file('imagen9'))
+                ->resize(144, 145)
+                ->save(public_path().'/imagenes/encuestas/' . $file->getClientOriginalName());
+            $encuesta->imagen9=$file->getClientOriginalName();
+        }
+        if(Input::hasFile('imagen10')) {
+            $file=Input::file('imagen10');
+            Image::make($request->file('imagen10'))
+                ->resize(144, 145)
+                ->save(public_path().'/imagenes/encuestas/' . $file->getClientOriginalName());
+            $encuesta->imagen10=$file->getClientOriginalName();
+        }
 
         if($encuesta -> save()){
             return redirect("/crear-encuesta");
